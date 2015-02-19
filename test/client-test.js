951d4c0f7ddb733d1e5777f314dbdf4cf0f1220f
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-02-19 15:53:08 -0500

    dns: mimic nodejs dns module's resolve methods

--- test/client-test.js
+++ test/client-test.js
@@ -4,17 +4,23 @@ var dgram = require('dgram');
 var dns = require('../');
 
 describe('dns.js client', function() {
-  var socket = dgram.createSocket('udp4');
-  var client = new dns.Client({
-    send: function(data) {
-      var buf = new Buffer(data);
-      socket.send(buf, 0, buf.length, 53, '8.8.8.8');
-    }
+  describe('it works with default DNS servers (Google)', function() {
+    runTests(); // Default is GoogleDNS @ 8.8.8.8
   });
 
-  socket.on('message', function(b) {
-    client.feed(b);
-  });
+  describe('it works with OpenDNS', function() {
+    runTests('208.67.222.222'); // OpenDNS
+  })
+});
+
+function runTests(dnsServer) {
+  var options = {};
+  if (dnsServer) {
+    options.dnsServer = dnsServer;
+    dns.Client.DEFAULT_DNS_SERVER = dnsServer
+  }
+
+  var client = new dns.Client(options);
 
   it('should send A queries', function(cb) {
     client.query('A', 'google.com', function(err, answers) {
@@ -29,7 +35,7 @@ describe('dns.js client', function() {
   });
 
   it('should send TXT queries', function(cb) {
-    client.query('TXT', 'blog.indutny.com', function(err, answers) {
+    client.query('TXT', 'google.com', function(err, answers) {
       assert(!err);
       assert(Array.isArray(answers));
       assert(answers.length > 0);
@@ -39,4 +45,44 @@ describe('dns.js client', function() {
       cb();
     });
   });
-});
+
+  it('closes socket on destroy, throws err if used after', function(cb) {
+    client.destroy();
+    var buf = new Buffer('something else');
+
+    assert(client.socket);
+    assert.throws(function() {
+      client.socket.send(buf, 0, buf.length, 53, '8.8.8.8');
+    }, Error);
+
+    assert.throws(function() {
+      client.resolve('google.com');
+    }, Error);
+
+    cb();
+  });
+
+  it('provides node\'s dns module\'s resolve method', function(cb) {
+    dns.resolve('google.com', function(err, addresses) {
+      assert(!err);
+      assert(Array.isArray(addresses));
+      assert(addresses.length > 0);
+      assert(addresses.every(function(a) {
+        return /^(\d{1,3}\.){3}\d{1,3}$/.test(a);
+      }));
+      cb();
+    });
+  });
+
+  it('provides node\'s dns module\'s resolve4 method', function(cb) {
+    dns.resolve4('google.com', function(err, addresses) {
+      assert(!err);
+      assert(Array.isArray(addresses));
+      assert(addresses.length > 0);
+      assert(addresses.every(function(a) {
+        return /^(\d{1,3}\.){3}\d{1,3}$/.test(a);
+      }));
+      cb();
+    });
+  });
+}
